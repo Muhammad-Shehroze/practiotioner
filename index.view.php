ad1105f2c8aec466dcc629613943e362c1d79533
Shehroze-Kamboh <shehroze511@gmail.com>
2021-01-19 18:01:19 +0500

    Lecture 6 Updated again

--- index.view.php
+++ index.view.php
@@ -8,13 +8,11 @@
 <body>
     <h1>
     <ul>
-        <?php
-            foreach ($names as $name)
-            {
-                echo "<li>$name</li>";
-            }
+        <?php foreach ($names as $name) : ?>
+                <li><?= $name; ?></li>
+        <?php endforeach; ?>    
 
-        ?>
+    </ul>
     
     </h1>
 </body>
