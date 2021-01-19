82ef6aaaa56fb2a894e1c829fd7716c6db42fd0a
Shehroze-Kamboh <shehroze511@gmail.com>
2021-01-19 17:39:46 +0500

    Lecture 5 Start

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        <?php
-            $name = $_GET['name'];
-            echo htmlspecialchars($name);
+<?php
 
-        ?>
-    
-    </h1>
-</body>
-</html>
+$greetings = "Hello John";
+require 'index.view.php';
