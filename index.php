a43e552649c70bc3b43e31b751b707b45edda917
Shehroze-Kamboh <shehroze511@gmail.com>
2021-01-19 17:25:28 +0500

    Lecture 4 Start

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-$greetings = "Hello";
-$name = "Shehroze";
-echo "{$greetings} , {$name}";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
